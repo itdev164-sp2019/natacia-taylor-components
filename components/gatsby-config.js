fd9f94719655de9713df025f5363f042b2bd0684
Natacia <taylorn7@gmatc.matc.edu>
2019-02-14 12:33:43 -0600

    created element components

--- components/gatsby-config.js
+++ components/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `ITDEV-164 Component Library`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
